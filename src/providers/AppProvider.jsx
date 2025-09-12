7662c1504fa77304701f94a4f619fc00638f8af3
Klara Frommelin <freakyfrommelin@outlook.com>
2025-09-12 10:22:11 +0200

    First iteration, creating app provider

---
+++ src/providers/AppProvider.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ConfigProvider from './ConfigProvider'
+import AuthProvider from './AuthProvider'
+import ApiProvider from './ApiProvider'
+
+const AppProvider = ( {children} ) => {
+  <ConfigProvider>
+    <AuthProvider>
+      <ApiProvider>
+        {children}
+      </ApiProvider>
+    </AuthProvider>
+  </ConfigProvider>
+}
+
+export default AppProvider
