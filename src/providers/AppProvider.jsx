b11edc4d0143d382232aa06ecca9cf0d6e9d2b35
Klara Frommelin <freakyfrommelin@outlook.com>
2025-09-23 12:18:52 +0200

    creating styles provider

--- src/providers/AppProvider.jsx
+++ src/providers/AppProvider.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import ConfigProvider from "./ConfigProvider";
 import AuthProvider from "./AuthProvider";
 import ApiProvider from "./ApiProvider";
+import StylesProvider from "./StylesProvider";
 
 const AppProvider = ({ children }) => (
   <ConfigProvider>
     <AuthProvider>
-      <ApiProvider>{children}</ApiProvider>
+      <ApiProvider>
+        <StylesProvider>{children}</StylesProvider>
+      </ApiProvider>
     </AuthProvider>
   </ConfigProvider>
 );
---
+++ src/providers/StylesProvider.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const StylesProvider = () => {};
+
+export default StylesProvider;
