5351cce8a7555141a09deb0a4c1e9d7f43e90405
jonatangroncode <jonatan.gron.code@hotmail.com>
2025-09-30 11:59:13 +0200

    use our components library in frontend

--- src/pages/Home/Home.page.jsx
+++ src/pages/Home/Home.page.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
-import styles from './home.module.css'
+import React from "react";
+import styles from "./home.module.css";
+import { GameBoard2 } from "femiral-components-lib";
+import "femiral-components-lib/style.css";
 
 const Home = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.heading}>Home Page</h1>
       <div className={styles.text}>
-        This is the page to write out the rules
+        <GameBoard2
+          size={7}
+          coloredCells={[{ row: 6, col: 1, color: "red" }]}
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
