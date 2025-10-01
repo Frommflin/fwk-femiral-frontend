c8f2275169eb517db84f0ad4a7af122d4d27ff07
jonatangroncode <jonatan.gron.code@hotmail.com>
2025-10-01 19:17:38 +0200

    gameboard in gamepage

--- src/pages/Game/Game.page.jsx
+++ src/pages/Game/Game.page.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./game.module.css";
+import { GameBoard2 } from "femiral-components-lib";
 
 const Game = () => {
   return (
@@ -7,7 +8,10 @@ const Game = () => {
       <div className={styles.container}>
         <h1 className={styles.heading}>Game Page</h1>
         <div className={styles.text}>
-          This is the page where the game is played.
+          <GameBoard2
+            size={7}
+            coloredCells={[{ row: 6, col: 1, color: "red" }]}
+          />
         </div>
       </div>
     </>
